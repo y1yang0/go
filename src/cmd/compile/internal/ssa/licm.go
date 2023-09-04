96a7e4699f7c117605dd8776d06bcfa0a31cf1c1
Yi Yang <y1yang0@qq.com>
2023-09-05 00:10:24 +0800

    beaut

--- src/cmd/compile/internal/ssa/licm.go
+++ src/cmd/compile/internal/ssa/licm.go
@@ -227,18 +227,18 @@ func (s *state) tryHoist(val *Value) bool {
 	// Hoist the loop-invariant panic check to loop land after rotation
 	// This simplifies CFG and allow more Value be hosited
 	//
-	//	                                    (Rotated Form...)
-	//	                                      bound check  <= loop land
-	//	     loop header                          |     ╲
-	//	       ╱   ▲   ╲                          │      ╲
-	//	      ╱     ╲   ╲                    loop header panic
-	//	  bound check╲ loop exit     =>       ▲      │
-	//	    ╱   ╲    ╱                        │      │
-	//	   ╱     ╲  ╱                        loop latch
-	//	panic   loop latch                       │
-	//	                                         |
-	//	                                      loop exit
 	//	                                   (Rotated Form...)
+	//	                                          bound check  <= loop land
+	//	       loop header                          ╱    ╲
+	//	        ╱   ▲   ╲                          ╱      ╲
+	//	       ╱     ╲   ╲                    loop header panic
+	//	  bound check ╲  loop exit     =>      ▲    ╲
+	//	    ╱   ╲     ╱                         ╲   ╱
+	//	   ╱     ╲   ╱                        loop latch
+	//	panic   loop latch                        │
+	//	                                          |
+	//	                                      loop exit
+	//	                                  (Rotated Form...)
 	if val.Block.Kind == BlockIf {
 	OOO:
 		for _, succ := range val.Block.Succs {
